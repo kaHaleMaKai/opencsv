7cc3fdfa8eb8f9c256eef75c9bf7a3d2f704bb64
Lars Winderling <lars.winderling@performance-advertising.de>
2016-07-12 11:05:22 +0200

    add registerDecoder() methods to CsvToBeanMapper interface

--- src/main/java/com/github/kahalemakai/opencsv/beans/CsvToBeanMapper.java
+++ src/main/java/com/github/kahalemakai/opencsv/beans/CsvToBeanMapper.java
@@ -15,7 +15,38 @@ public interface CsvToBeanMapper<T> extends Closeable, Iterable<BeanAccessor<T>>
         return withLines(() -> lines);
     }
 
-    default void registerDecoder(String column, Decoder<?> decoder) { }
+    // should be override by subclasses
+    default CsvToBeanMapper<T> registerDecoder(String column, Decoder<?, ? extends Throwable> decoder) {
+        return this;
+    }
+
+    // should be override by subclasses
+    default CsvToBeanMapper<T> registerDecoder(String column,
+                                               Class<? extends Decoder<?, ? extends Throwable>> decoderClass) throws InstantiationException {
+        return this;
+    }
+
+    @SuppressWarnings("unchecked")
+    default CsvToBeanMapper<T> registerDecoder(String column,
+                                               Decoder<?, ? extends Throwable> decoder,
+                                               Decoder<?, ? extends Throwable>...decoders) {
+        registerDecoder(column, decoder);
+        for (Decoder<?, ? extends Throwable> d : decoders) {
+            registerDecoder(column, d);
+        }
+        return this;
+    }
+
+    @SuppressWarnings("unchecked")
+    default CsvToBeanMapper<T> registerDecoder(String column,
+                                               Class<? extends Decoder<?, ? extends Throwable>> decoderClass,
+                                               Class<? extends Decoder<?, ? extends Throwable>>...decoderClasses) throws InstantiationException {
+        registerDecoder(column, decoderClass);
+        for (Class<? extends Decoder<?, ? extends Throwable>> aClass : decoderClasses) {
+            registerDecoder(column, aClass);
+        }
+        return this;
+    }
 
     Class<? extends T> getType();
 
