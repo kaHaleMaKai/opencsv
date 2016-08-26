7ef589f29720a0e32c5203997b5154b5ec6b0176
Lars Winderling <lars.winderling@performance-advertising.de>
2016-08-26 20:48:43 +0200

    add javadocs to ResultWrapper subclasses

--- src/main/java/com/github/kahalemakai/opencsv/beans/processing/ResultWrapperError.java
+++ src/main/java/com/github/kahalemakai/opencsv/beans/processing/ResultWrapperError.java
@@ -3,20 +3,45 @@ package com.github.kahalemakai.opencsv.beans.processing;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Singleton representing an error state.
+ * <p>
+ * {@link #success()} always return false and
+ * {@link #get()} throws an {@code UnsupportedOperationException}.
+ * @param <T> type of wrapped object, only important for type inference
+ */
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-public class ResultWrapperError<T> implements ResultWrapper<T> {
+class ResultWrapperError<T> implements ResultWrapper<T> {
+    /**
+     * The singleton object
+     */
     private static final ResultWrapperError<?> ERROR = new ResultWrapperError<>();
 
+    /**
+     * Always return {@code false}, indicating a state of error.
+     * @return {@code false}
+     */
     @Override
     public boolean success() {
         return false;
     }
 
+    /**
+     * Throw an exception, because this class indicates
+     * an error state, i.e. the absence of a wrapped value.
+     * @return this method only throws
+     * @throws UnsupportedOperationException on method call
+     */
     @Override
-    public T get() {
+    public T get() throws UnsupportedOperationException {
         throw new UnsupportedOperationException("trying to get value of error indicating object wrapper");
     }
 
+    /**
+     * Return the singleton instance.
+     * @param <S> type of wrapped object, only important for type inference
+     * @return the error indicating singleton instance
+     */
     public static <S> ResultWrapper<S> error() {
         @SuppressWarnings("unchecked")
         final ResultWrapper<S> error = (ResultWrapper<S>) ERROR;
--- src/main/java/com/github/kahalemakai/opencsv/beans/processing/ResultWrapperImpl.java
+++ src/main/java/com/github/kahalemakai/opencsv/beans/processing/ResultWrapperImpl.java
@@ -2,18 +2,38 @@ package com.github.kahalemakai.opencsv.beans.processing;
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Return a {@code ResultWrapper} indicating a state of success.
+ * <p>
+ * Instances of this class <i>always</i> contain a wrapped object.
+ * @param <T> type of wrapped object
+ */
 @RequiredArgsConstructor(staticName = "of")
 class ResultWrapperImpl<T> implements ResultWrapper<T> {
+    /**
+     * the wrapped object
+     */
     private final T value;
-    final static ResultWrapper<?> NULL_WRAPPER = ResultWrapperImpl.of(null);
-    final static ResultWrapper<Boolean> TRUE = ResultWrapperImpl.of(true);
-    final static ResultWrapper<Boolean> FALSE = ResultWrapperImpl.of(false);
 
+    /**
+     * Instance wrapped around a {@code null} reference.
+     * This object can be obtained by {@link ResultWrapper#ofNull()}.
+     */
+    final static ResultWrapper<?> NULL_WRAPPER = ResultWrapperImpl.of(null);
+
+    /**
+     * Always return {@code true}.
+     * @return {@code true}
+     */
     @Override
     public boolean success() {
         return true;
     }
 
+    /**
+     * Return the wrapped object.
+     * @return the wrapped object
+     */
     @Override
     public T get() {
         return value;
