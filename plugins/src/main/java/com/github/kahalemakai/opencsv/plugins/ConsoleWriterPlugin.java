a9fb929e846b5cf381e9f77a25e38e43890c241e
Lars Winderling <lars.winderling@performance-advertising.de>
2016-09-10 19:46:38 +0200

    update console-writer

--- plugins/src/main/java/com/github/kahalemakai/opencsv/plugins/ConsoleWriterPlugin.java
+++ plugins/src/main/java/com/github/kahalemakai/opencsv/plugins/ConsoleWriterPlugin.java
@@ -2,6 +2,7 @@ package com.github.kahalemakai.opencsv.plugins;
 
 import com.github.kahalemakai.opencsv.beans.Builder;
 import com.github.kahalemakai.opencsv.config.Plugin;
+import com.github.kahalemakai.opencsv.config.Sink;
 import com.github.kahalemakai.opencsv.config.SinkPlugin;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,14 +12,13 @@ import org.w3c.dom.Node;
 import java.io.File;
 import java.util.Iterator;
 import java.util.Optional;
-import java.util.function.Consumer;
 
 @RequiredArgsConstructor(staticName = "init")
 @Log4j
 public class ConsoleWriterPlugin implements SinkPlugin {
     private static final String NAMESPACE = "http://github.com/kaHaleMaKai/opencsv/plugins/console-writer";
     @Getter
-    Consumer<Iterator<?>> sink;
+    Sink sink;
 
     @Override
     public File getSchemaFile() {
